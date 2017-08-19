399fd9138fd3ff450ebe4ebcb736ed9f6c57f1d3
Alex <alexgascon.93@gmail.com>
2017-08-20 00:02:05 +0200

    Updated announcement model

--- src/models.py
+++ src/models.py
@@ -3,8 +3,6 @@ class Announcement:
     """Announcement posted in the official KHUXNA site
     (currently, http://api.sp.kingdomhearts.com/information/list) """
 
-    def __init__(self, id, title, link, date_posted):
+    def __init__(self, id, title):
         self.id = id
         self.title = title
-        self.link = link
-        self.date_posted = date_posted
