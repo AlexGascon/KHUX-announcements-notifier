d556ad6c314678327984bc5c7b0f5d08ac2f8c17
Alex <alexgascon.93@gmail.com>
2017-08-20 21:13:19 +0200

    Constants added

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,7 @@
-ANNOUNCEMENTS_URL = "http://api.sp.kingdomhearts.com/information/list"
+BASE_URL = "http://api.sp.kingdomhearts.com/"
+ANNOUNCEMENT_URL = BASE_URL + "information/list"
+
+BOT_USER_AGENT = "KHUX_notice_bot by /u/Pawah"
+BOT_USERNAME = "khux_notice_bot"
+
+SUBREDDIT_TO_USE = "test"  # Starting with /r/test subreddit
