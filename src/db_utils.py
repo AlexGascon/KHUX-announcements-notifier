09bb02e4f2def5928dce4d21c125ef3003783b27
Alex <alexgascon.93@gmail.com>
2017-08-23 20:14:47 +0200

    Created db_utils
    Created functions to simplify DB operations

---
+++ src/db_utils.py
@@ -0,0 +1,20 @@
+import os
+import pymongo
+
+from src.constants import DB_COLLECTION_NAME
+
+
+def get_database():
+    """Connects to the DB and returns it"""
+    connection = pymongo.MongoClient(os.environ.get("DB_CONNECTION_STRING"))
+    db = connection["DB_NAME"]
+
+    return db
+
+
+def get_collection():
+    """Gets the collection we'll use to store the information and returns it"""
+    db = get_database()
+    collection = db[DB_COLLECTION_NAME]
+
+    return collection
