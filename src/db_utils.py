6380076da7e1ab8f893447103069c5ab8f8d633f
Alex <alexgascon.93@gmail.com>
2017-08-24 18:10:33 +0200

    Created method "insert_announcements"

--- src/db_utils.py
+++ src/db_utils.py
@@ -18,3 +18,18 @@ def get_collection():
     collection = db[DB_COLLECTION_NAME]
 
     return collection
+
+
+def insert_announcements(announcements):
+    """Inserts the announcements into the database"""
+    # TODO: Check if there's any way of reducing dependencies on this method.
+    # Currently, it relies very heavily on the specific operation we want to
+    # do and the structure of Announcement. This may change in the future
+    # along with our requirements
+
+    collection = get_collection()
+
+    update_content = {"$setOnInsert": {"posted": False}}
+    for announcement in announcements:
+        collection.update(announcement.to_mongo(), update_content, upsert=True)
+
