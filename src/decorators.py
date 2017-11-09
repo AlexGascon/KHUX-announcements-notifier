f8e02effe04490c991f12de3767503e8aedd6710
Alex <alexgascon.93@gmail.com>
2017-11-09 18:33:58 +0100

    Documenting logger

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,7 @@
 def logger(original_function):
+    """Decorator that will log execution information of the method.
+    Currently, it logs its name, arguments with which it has been run and execution time"""
+    
     import time
 
     def wrapper_function(*args, **kwargs):
