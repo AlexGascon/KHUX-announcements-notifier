365576f309359ce6d03b1a2b9762d42f08299edd
Alex <alexgascon.93@gmail.com>
2017-11-09 18:57:11 +0100

    Bug fixing

--- src/decorators.py
+++ src/decorators.py
@@ -5,8 +5,9 @@ def logger(original_function):
     import time
 
     def wrapper_function(*args, **kwargs):
-        print("Entering {}.\n Ran with arguments {} \nand keyword arguments {}").format(original_function.__name__,
-                                                                                        args, kwargs)
+        print("Entering {}.\n Ran with arguments {} \nand keyword arguments {}".format(original_function.__name__,
+                                                                                       args,
+                                                                                       kwargs))
         start_time = time.now
 
         result = original_function(*args, **kwargs)
