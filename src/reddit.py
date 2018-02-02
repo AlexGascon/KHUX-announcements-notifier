149645a937a2b87f6cbd4f9e1a456e3325ac0833
Alex <alexgascon.93@gmail.com>
2018-02-02 20:04:36 +0100

    Refactoring post_announcement and comment_in_submission

--- src/reddit.py
+++ src/reddit.py
@@ -2,9 +2,10 @@ import os
 
 import praw
 
-from src.constants import BOT_USER_AGENT, BOT_USERNAME, BOT_COMMENT
+from src.constants import BOT_USER_AGENT, BOT_USERNAME
 from src.db_utils import mark_announcement_as_posted
 from src.decorators import logger
+
 
 @logger
 def get_subreddit():
@@ -18,6 +19,7 @@ def get_subreddit():
                          password=os.environ.get("BOT_PASSWORD"))
     subreddit = reddit.subreddit(os.environ.get("SUBREDDIT"))
     return subreddit
+
 
 @logger
 def post_announcement(announcement):
@@ -37,17 +39,12 @@ def post_announcement(announcement):
         print("Posted announcement {}".format(announcement))
         
         if submission:
-            BOT_COMMENT = "Beeeeep bop. I'm a bot! I post SENA's announcements to /r/" + os.environ.get("SUBREDDIT") + \
-                          "\nIf you want to know more about me, you can see my code on " + \
-                          "[Github](https://github.com/AlexGascon/KHUX-announcements-notifier) or contact " + \
-                          "my creator [/u/Pawah](https://www.reddit.com/user/pawah)"
-
             comment_in_submission(submission)
 
         return submission
 
-    except praw.exceptions.APIException as e:
-        print ("API Rate Limit error: " + e.message)
+    except Exception as e:
+        print ("Error while posting announcement: " + e.message)
 
         # In the first stages of the bot, we will simply ignore the unposted announcements.
         # Posting them later could be unnecessary spam.
@@ -56,9 +53,13 @@ def post_announcement(announcement):
 
         return None
 
+
 @logger
 def comment_in_submission(submission):
     """Comments after submitting a post to indicate that OP's a robot"""
-    return submission.reply(BOT_COMMENT)
+    bot_comment = "Beeeeep bop. I'm a bot! I post SENA's announcements to /r/" + os.environ.get("SUBREDDIT") + \
+                  "\nIf you want to know more about me, you can see my code on " + \
+                  "[Github](https://github.com/AlexGascon/KHUX-announcements-notifier) or contact " + \
+                  "my creator [/u/Pawah](https://www.reddit.com/user/pawah)"
 
-
+    return submission.reply(bot_comment)
